6e342229f6dd58a80875c2c32c8ba8c1c4fd10d5
snaipp1 <snaipp1@yandex.by>
2021-09-11 19:02:08 +0300

    feat: add authorization

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const userController = require('../Controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 const router = new Router();
 
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
-router.get('/auth', userController.check);
+router.get('/auth', authMiddleware, userController.check);
 
 module.exports = router;
