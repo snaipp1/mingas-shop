bcdd6119d6f562ff25b553e949941a6e7a741d53
snaipp1 <snaipp1@yandex.by>
2021-09-11 15:24:35 +0300

    feat: add base backend logic

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 const router = require('./routes/index');
 
 const PORT = process.env.PORT || 5000;
@@ -12,6 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
 
