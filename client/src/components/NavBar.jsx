0b02858371d4d8697ee506ca55a8d58766ef7db8
snaipp1 <snaipp1@yandex.by>
2021-09-12 20:06:34 +0300

    feat: add modals for admin page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,21 +4,34 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { NavLink } from 'react-router-dom';
-import { SHOP_ROUTE } from '../utils/consts';
+import { SHOP_ROUTE, ADMIN_ROUTE, LOGIN_ROUTE } from '../utils/consts';
 import {Button} from "react-bootstrap";
 import {observer} from 'mobx-react-lite';
+import { useHistory } from 'react-router-dom';
 
 
 const NavBar = observer(() => {
     const {user} = useContext(Context);
+    const history = useHistory();
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color: "#fff"}} to={SHOP_ROUTE}>Название магазина</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color: "#fff"}}>
-                        <Button variant={"outline-light"}>Административная панель</Button>
-                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(false)} style={{marginLeft: '5px'}}>Выйти</Button>
+                        <Button 
+                            variant={"outline-light"} 
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Административная панель
+                        </Button>
+                        <Button 
+                            variant={"outline-light"} 
+                            onClick={() => history.push(LOGIN_ROUTE)} 
+                            style={{marginLeft: '5px'}}
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
                      <Nav className="ml-auto" style={{color: "#fff"}}>
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+import CreateBrand from '../components/modals/CreateBrand';
+import CreateDevice from '../components/modals/CreateDevice';
+import CreateType from '../components/modals/CreateType';
 
 const Admin = () => {
     return (
-        <div>
-            Admin
-        </div>
+        <Container className="d-flex flex-column">
+            <Button 
+                variant={"outline-dark"} 
+                className="mt-2 p-2"
+            >
+                    Добавить тип
+            </Button>
+            <Button 
+                variant={"outline-dark"} 
+                className="mt-2 p-2"
+            >
+                    Добавить бренд
+            </Button>
+            <Button 
+                variant={"outline-dark"} 
+                className="mt-2 p-2"
+            >
+                    Добавить устройство
+            </Button>
+            <CreateType show={true}/>
+            <CreateBrand/>
+            <CreateDevice/>
+        </Container>
     )
 };
 
